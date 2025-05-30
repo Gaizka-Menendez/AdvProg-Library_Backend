0cd5a794cc15313b049901ca7704984bcd6384ea
Gaizka Menéndez Hernández <gaizkam20@gmail.com>
2025-05-30 20:00:27 +0200

    Abstaract class definition

--- Biblio_Digi_Pract/source_code/models.py
+++ Biblio_Digi_Pract/source_code/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from .database import * 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, property
 
 
 
@@ -18,17 +18,52 @@ class UserDB(Base):
 
 # La idea es que tanto CD como los libros tengan atributos en comun y aplicando herencia posteriormente cada uno tenga sus particularidades
 class Library_Item(ABC):
-    pass
+    
+    @abstractmethod
+    def __init__(self, name: str, nb: int):
+        self._ref_number = nb
+        self._name = name
+        self._available = True
+       
+    @property
+    def get_item_reference_numb(self):
+        return self._ref_number
+    
+    @property
+    def get_name(self):
+        return self._name
+    
+    @property
+    def get_status(self):
+        return self._available # si esta disponible o no en nuestra web para alquilar este item
+    
+    def item_took(self):
+        self.available = False
+    
+    def item_returned(self):
+        self.available = True
+
+
+class Dvd_DB(Base, Library_Item):
+    
+    __tablename__ = "dvds"
+    
+    ref_number = Column(Integer, primary_key=True, index=True)
+    # It is typically not desirable to have “autoincrement” enabled on a column that refers to another via foreign key, as such a column is required to refer to a value that originates from elsewhere.
+    name = Column(String, unique=True, index=True)
+    actors = Column(list(String), index=True)
+    available = Column(Boolean, default=True) # Inicialmente lo declaramos a true porque si lo hemos añadido a la BDD es porque lo tenemos
+    date_registered = Column(DateTime(timezone=True), server_default=func.now())
 
 
 
-class BookDB(Base):
+class Book_DB(Base, Library_Item):
     
     __tablename__ = "books"
     
     ref_number = Column(Integer, primary_key=True, index=True)
     # It is typically not desirable to have “autoincrement” enabled on a column that refers to another via foreign key, as such a column is required to refer to a value that originates from elsewhere.
-    b_name = Column(String, unique=True, index=True)
+    name = Column(String, unique=True, index=True)
     author = Column(String, index=True)
     available = Column(Boolean, default=True) # Inicialmente lo declaramos a true porque si lo hemos añadido a la BDD es porque lo tenemos
     date_registered = Column(DateTime(timezone=True), server_default=func.now())
