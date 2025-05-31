c77e0bd6b0a08daf3c5fde674633ad31cc9e0951
Gaizka Menéndez Hernández <gaizkam20@gmail.com>
2025-05-31 19:48:09 +0200

    init and some getters methods

--- Biblio_Digi_Pract/source_code/models.py
+++ Biblio_Digi_Pract/source_code/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, T
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from .database import * 
-from abc import ABC, abstractmethod, property
+from abc import ABC, abstractmethod
 
 
 
@@ -11,40 +11,50 @@ class UserDB(Base):
     __tablename__ = "users"
     
     user_id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, unique=True, index=True, nullable=False)
     date_added = Column(DateTime(timezone=True), server_default=func.now())
-    hashed_password = Column(String)
-    contact_mail = Column(String, unique=True, index=True)
-    age = Column(Integer, index=True)
+    hashed_password = Column(String, nullable=False)
+    contact_mail = Column(String, unique=True, index=True, nullable=False)
+    age = Column(Integer, index=True, nullable=True)
     
     loans = relationship("Loan_DB", back_populates="user_resp")
     
+    
+    def __init__(self, name: str, mail: str, passwd: str,  age: int = None):
+        self.full_name = name
+        self.contact_mail = mail
+        self.age = age
+        self.hashed_password = passwd
+    
 
-# La idea es que tanto CD como los libros tengan atributos en comun y aplicando herencia posteriormente cada uno tenga sus particularidades
+# La idea es que tanto las peliculas como los libros tengan atributos en comun y aplicando herencia posteriormente cada uno tenga sus particularidades
 class Library_Item(ABC):
     
-    @abstractmethod
-    def __init__(self, name: str, nb: int):
-        self._ref_number = nb
+    def __init__(self, name: str):
         self._name = name
         self._available = True
-       
-    @property
-    def get_item_reference_numb(self):
-        return self._ref_number
     
     @property
-    def get_name(self):
+    def name(self):
         return self._name
     
     @property
-    def get_status(self):
+    def status(self):
         return self._available # si esta disponible o no en nuestra web para alquilar este item
     
     def item_took(self):
-        self.available = False
+        self._available = False
     
     def item_returned(self):
-        self.available = True
+        self._available = True
+        
+    @abstractmethod
+    def get_item_type(self):
+        pass # para decir que está aquilando el usuario
+    
+    @abstractmethod
+    def get_ref_numb(self):
+        pass
 
 
 class Film_DB(Base, Library_Item):
@@ -54,13 +64,35 @@ class Film_DB(Base, Library_Item):
     ref_number = Column(Integer, primary_key=True, index=True)
     # It is typically not desirable to have “autoincrement” enabled on a column that refers to another via foreign key, as such a column is required to refer to a value that originates from elsewhere.
     name = Column(String, unique=True, index=True)
-    actors = Column(String, index=True)
-    available = Column(Boolean, default=True) # Inicialmente lo declaramos a true porque si lo hemos añadido a la BDD es porque lo tenemos
+    actors = Column(String, nullable=True) # Aquí hemos quitado el indice (index=True) porque la lista de actores se pasara como un string donde vendran separados por comas
+    available = Column(Boolean, default=True, nullable=False) # Inicialmente lo declaramos a true porque si lo hemos añadido a la BDD es porque lo tenemos
     date_registered = Column(DateTime(timezone=True), server_default=func.now())
     
-    genres = relationship("Gender_DB", secondary="film_gender_association", back_populates="films")
+    genres = relationship("Genre_DB", secondary="film_genre_association", back_populates="films")
     loans = relationship("Loan_DB", back_populates="film_loaned")
-
+    
+    
+    def __init__(self, name: str, actors: str):
+        super().__init__(name)
+        self.actors = actors
+       
+    def get_item_type(self):
+        return "Film"
+    
+    def get_ref_numb(self):
+        return self.ref_number
+    
+    def actors_listing(self):
+        actors_list = self.actors.split(",")
+        l = dict()
+        if actors_list:
+            elem_added = 0
+            l["Info"] = "Film casting"
+            for a in actors_list:
+                l[f"A{elem_added}"] = a.strip()
+                elem_added +=1
+        return l   
+        
 
 
 class Book_DB(Base, Library_Item):
@@ -71,11 +103,22 @@ class Book_DB(Base, Library_Item):
     # It is typically not desirable to have “autoincrement” enabled on a column that refers to another via foreign key, as such a column is required to refer to a value that originates from elsewhere.
     name = Column(String, unique=True, index=True)
     author = Column(String, index=True)
-    available = Column(Boolean, default=True) # Inicialmente lo declaramos a true porque si lo hemos añadido a la BDD es porque lo tenemos
+    available = Column(Boolean, default=True, nullable=False) # Inicialmente lo declaramos a true porque si lo hemos añadido a la BDD es porque lo tenemos
     date_registered = Column(DateTime(timezone=True), server_default=func.now())
     
-    genres = relationship("Genre_DB", secondary="book_gender_association", back_populates="books")
+    genres = relationship("Genre_DB", secondary="book_genre_association", back_populates="books")
     loans = relationship("Loan_DB", back_populates="book_loaned")
+    
+    
+    def __init__(self, name: str, author: str):
+        super().__init__(name)
+        self.author = author
+              
+    def get_item_type(self):
+        return "Book"
+    
+    def get_ref_numb(self):
+        return self.ref_number
     
 
 class Genre_DB(Base):
@@ -83,10 +126,14 @@ class Genre_DB(Base):
     __tablename__ = "genero"
     
     genre_id = Column(Integer, primary_key=True, index=True)
-    genre_name = Column(String, unique=True, index=True)
+    genre_name = Column(String, unique=True, index=True, nullable=False)
 
-    books = relationship("Book_DB", secondary="book_gender_association", back_populates="genres")
-    films = relationship("Film_DB", secondary="film_gender_association", back_populates="genres")
+    books = relationship("Book_DB", secondary="book_genre_association", back_populates="genres")
+    films = relationship("Film_DB", secondary="film_genre_association", back_populates="genres")
+    
+    
+    def __init__(self, name):
+        self.genre_name = name
 
     
 
@@ -98,29 +145,28 @@ class Loan_DB(Base):
     loan_id = Column(Integer, primary_key=True, index=True)
     loan_date = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False) # Un préstamo siempre tiene un usuario
-
     book_ref_number = Column(Integer, ForeignKey("books.ref_number"), nullable=True)
     film_ref_number = Column(Integer, ForeignKey("films.ref_number"), nullable=True)
+    
     user_responsible = relationship("UserDB", back_populates="loans")
     book_loaned = relationship("Book_DB", back_populates="loans")
     film_loaned = relationship("Film_DB", back_populates="loans")
-
+    
 
 
 # Estas tablas para las relaciones N:M de mi diagrama me he servido de Gemini para generarlas
+# Tablas de asociación Many to Many
     
-film_gender_association_table = Table(
-        "film_gender_association", Base.metadata,
+film_genre_association_table = Table(
+        "film_genre_association", Base.metadata,
         Column("film_ref_number", Integer, ForeignKey("films.ref_number"), primary_key=True),
-        Column("gender_id", Integer, ForeignKey("genero.gender_id"), primary_key=True)
+        Column("genre_id", Integer, ForeignKey("genero.genre_id"), primary_key=True)
     )
     
     
-book_gender_association_table = Table(
-        "book_gender_association", Base.metadata,
+book_genre_association_table = Table(
+        "book_genre_association", Base.metadata,
         Column("book_ref_number", Integer, ForeignKey("books.ref_number"), primary_key=True),
-        Column("gender_id", Integer, ForeignKey("genero.gender_id"), primary_key=True)
+        Column("genre_id", Integer, ForeignKey("genero.genre_id"), primary_key=True)
     )
     
-    
-Base.metadata.create_all(bind=engine)
